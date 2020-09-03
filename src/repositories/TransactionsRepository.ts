862e0233f2b75b98332126dcfe75dc95677cdb99
daniel-guarino <daniel.gguarino@gmail.com>
2020-09-02 21:15:19 -0300

    criacao da Rota transactions, balanco total das transacoes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,12 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +21,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    if (this.transactions.length === 0) {
+      throw Error("You don't have any transaction");
+    }
+
+    return this.transactions
+
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((accumulated, totalAccumuled) => accumulated + totalAccumuled.value,0);
+
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((accumulated, totalAccumulated) => accumulated + totalAccumulated.value,0);
+
+    const total = income - outcome;
+
+    const balance : Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }:CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+
+
   }
 }
 
